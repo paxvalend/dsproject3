9d6243dd3684d42b970df10220bbcfa5799a16db
unknown <paxvalend700@gmail.com>
2022-04-18 16:37:16 +0700

    sql connection

---
+++ connection/mysql.py
@@ -0,0 +1,23 @@
+# IMPORT MODULE
+import json
+import pandas as pd
+import numpy as np
+
+# IMPORT SCRIPT
+from sqlalchemy import create_engine
+
+
+class MySQL:
+  def __init__(self, cfg):
+    self.host = cfg['host']
+    self.port = cfg['port']
+    self.username = cfg['username']
+    self.password = cfg['password']
+    self.database = cfg['database']
+
+  def connect(self):
+    engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(self.username, self.password, self.host, self.port, self.database))
+    engine_conn = engine.connect()
+    print("Connect Engine MySQL")
+    return engine, engine_conn
+
